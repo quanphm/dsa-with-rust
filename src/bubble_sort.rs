d33623b57728898738ecac3601952a0c31780d3b
Quan Pham <6479147+mikunpham@users.noreply.github.com>
2024-01-30 18:14:43 +0700

    update bubble_sort

--- src/bubble_sort.rs
+++ src/bubble_sort.rs
@@ -3,7 +3,7 @@ where
     T: PartialOrd + Clone,
 {
     let length = collection.len();
-    for i in 0..length {
+    for i in 0..(length - 1) {
         for j in 0..(length - i - 1) {
             if collection[j] > collection[j + 1] {
                 collection.swap(j, j + 1)
@@ -21,11 +21,5 @@ mod tests {
     fn should_return_sorted_array() {
         let mut arr = [2, 1, 4, 3, 5];
         assert_eq!(vec![1, 2, 3, 4, 5], bubble_sort(&mut arr));
-
-        let mut arr = vec![2, 1, 1, 0, 3];
-        assert_eq!(vec![0, 1, 1, 2, 3], bubble_sort(&mut arr));
-
-        let mut arr = ['b', 'd', 'a', 'c'];
-        assert_eq!(vec!['a', 'b', 'c', 'd'], bubble_sort(&mut arr[..]));
     }
 }
