86f39f10a0db1629af3b907277849f81bae770eb
Quan Pham <6479147+mikunpham@users.noreply.github.com>
2024-01-30 17:55:56 +0700

    mess around

--- src/bubble_sort.rs
+++ src/bubble_sort.rs
@@ -1,17 +1,16 @@
-pub fn bubble_sort<T>(collection: &[T]) -> Vec<T>
+pub fn bubble_sort<T>(collection: &mut [T]) -> Vec<T>
 where
-    T: Clone + PartialOrd,
+    T: PartialOrd + Clone,
 {
     let length = collection.len();
-    let mut result: Vec<T> = collection.into();
-    for i in 0..(length - 1) {
+    for i in 0..length {
         for j in 0..(length - i - 1) {
-            if result[j] > result[j + 1] {
-                result.swap(j, j + 1)
+            if collection[j] > collection[j + 1] {
+                collection.swap(j, j + 1)
             }
         }
     }
-    result
+    collection.to_vec()
 }
 
 #[cfg(test)]
@@ -20,13 +19,13 @@ mod tests {
 
     #[test]
     fn should_return_sorted_array() {
-        let arr = [2, 1, 4, 3, 5];
-        assert_eq!(vec![1, 2, 3, 4, 5], bubble_sort(&arr));
+        let mut arr = [2, 1, 4, 3, 5];
+        assert_eq!(vec![1, 2, 3, 4, 5], bubble_sort(&mut arr));
 
-        let arr = [2, 1, 1, 0, 3];
-        assert_eq!(vec![0, 1, 1, 2, 3], bubble_sort(&arr));
+        let mut arr = vec![2, 1, 1, 0, 3];
+        assert_eq!(vec![0, 1, 1, 2, 3], bubble_sort(&mut arr));
 
-        let arr = ['b', 'd', 'a', 'c'];
-        assert_eq!(vec!['a', 'b', 'c', 'd'], bubble_sort(&arr));
+        let mut arr = ['b', 'd', 'a', 'c'];
+        assert_eq!(vec!['a', 'b', 'c', 'd'], bubble_sort(&mut arr[..]));
     }
 }
