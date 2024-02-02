9691de523c6fa8a17be5b78a25cee4d36a1871ba
Quan Pham <6479147+mikunpham@users.noreply.github.com>
2024-02-02 13:17:03 +0700

    try to understand borrow and mutable

--- src/insertion_sort.rs
+++ src/insertion_sort.rs
@@ -1,9 +1,5 @@
-pub fn insertion_sort<T>(collection: &mut [T]) -> Vec<T>
-where
-    T: PartialOrd + Clone,
-{
+pub fn insertion_sort<T: PartialOrd>(collection: &mut [T]) {
     let len = collection.len();
-
     for i in 1..len {
         let mut j = i;
         while j > 0 && collection[j - 1] > collection[j] {
@@ -11,8 +7,6 @@ where
             j -= 1;
         }
     }
-
-    collection.to_vec()
 }
 
 #[cfg(test)]
@@ -22,12 +16,16 @@ mod tests {
     #[test]
     fn should_return_sorted_array() {
         let mut arr = [2, 1, 4, 3, 5];
-        assert_eq!(vec![1, 2, 3, 4, 5], insertion_sort(&mut arr));
+        insertion_sort(&mut arr);
+        assert_eq!([1, 2, 3, 4, 5], arr);
 
         let mut arr = vec![2, 1, 1, 0, 3];
-        assert_eq!(vec![0, 1, 1, 2, 3], insertion_sort(&mut arr));
+        insertion_sort(&mut arr);
+        assert_eq!(vec![0, 1, 1, 2, 3], arr);
 
         let mut arr = ['b', 'd', 'a', 'c'];
-        assert_eq!(vec!['a', 'b', 'c', 'd'], insertion_sort(&mut arr[..]));
+        let mut slice = &mut arr[0..3];
+        insertion_sort(&mut slice);
+        assert_eq!(['a', 'b', 'd'], slice);
     }
 }
